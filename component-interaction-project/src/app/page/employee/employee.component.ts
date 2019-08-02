f85a8ab4fd79dcadfba36994fcff3cc55e095c7a
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-08-02 18:03:53 +0200

    blah

--- component-interaction-project/src/app/page/employee/employee.component.ts
+++ component-interaction-project/src/app/page/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MockData } from '../../model/mock-data';
 import { Employee } from '../../model/employee'
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
@@ -10,8 +11,13 @@ import { Employee } from '../../model/employee'
 export class EmployeeComponent implements OnInit {
   data: MockData = new MockData();
   employees: Employee[] = this.data.employee;
-  
-  
+  title: string = 'Employees'
+  keyz: string[];
+  tableHeadKeys(): void {
+    this.keyz = Object.keys(this.employees[0]);
+    this.keyz = this.keyz.slice(1)
+    console.log(this.keyz);
+  };
   constructor() { }
 
   ngOnInit() {
