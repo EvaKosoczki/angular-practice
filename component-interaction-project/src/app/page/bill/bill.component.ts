19fbbfaf92d0182696f90649034f075f5de7db12
Daniel Olah <olah.daniel95@gmail.com>
2019-08-05 11:25:17 +0200

    Bugfix: loading bug

--- component-interaction-project/src/app/page/bill/bill.component.ts
+++ component-interaction-project/src/app/page/bill/bill.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Bill } from '../../model/bill';
 import { MockData } from '../../model/mock-data';
-import { ModalComponent } from '../../modal/modal.component';
 
 @Component({
   selector: 'app-bill',
@@ -13,23 +12,16 @@ export class BillComponent implements OnInit {
   title: string = `Bills`;
   billData: Bill[] = new MockData().bills;
   billKeys: string[] = Object.keys(this.billData[0]);
-  billSpecs: Bill;
+  index: number = -1;
+  longID: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  // getBillSpecs(event): void {
-  //   let i = 0;
-  //   const parentElement = event.target.parentNode;
-  //   while(i < parentElement.children.length) {
-  //     if(event.target === parentElement.children[i]) {
-  //       break;
-  //     }
-  //     i += 1;
-  //   } 
-  //   this.billSpecs = this.billData[i];
-  // }
+  toggleID(): void {
+    this.longID = !this.longID;
+  }
 
 }
--- component-interaction-project/src/app/page/bill/bill.idpipe.component.ts
+++ component-interaction-project/src/app/page/bill/bill.idpipe.component.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IDPipe implements PipeTransform {
   transform(value: string): string {
-    return `${value.slice(0, 3)}...${value.slice(value.length-3, value.length)}`;
+    return `${value.slice(0, 3)}...${value.slice(value.length - 3, value.length)}`;
   }
 }
