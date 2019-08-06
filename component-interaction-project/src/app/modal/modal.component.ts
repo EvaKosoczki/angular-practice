05577f7827020efcb268b83a0e10ac4e20dd5d14
Daniel Olah <olah.daniel95@gmail.com>
2019-08-06 09:49:40 +0200

    Commented modal files

--- component-interaction-project/src/app/modal/modal.component.ts
+++ component-interaction-project/src/app/modal/modal.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Bill } from '../model/bill';
+// import { Component, OnInit, Input } from '@angular/core';
+// import { Bill } from '../model/bill';
 
-@Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
-})
-export class ModalComponent implements OnInit {
+// @Component({
+//   selector: 'app-modal',
+//   templateUrl: './modal.component.html',
+//   styleUrls: ['./modal.component.css']
+// })
+// export class ModalComponent implements OnInit {
 
-  @Input('bill') bill: Bill;
-  title: string = `Bill Specs`;
+//   @Input('bill') bill: Bill;
+//   title: string = `Bill Specs`;
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-  showMsg(): void {
-    console.log(this.bill);
-    this.show = true;
-  }
+//   showMsg(): void {
+//     console.log(this.bill);
+//     this.show = true;
+//   }
 
-  hide(): void {
-    this.show = false;
-  }
+//   hide(): void {
+//     this.show = false;
+//   }
 
-  getDisplay(): string {
-    return this.show ? 'block' : 'none';
-  }
+//   getDisplay(): string {
+//     return this.show ? 'block' : 'none';
+//   }
 
-}
+// }
